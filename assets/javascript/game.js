16456e096462116918614ee2fd2391c51c7d491f
Piper <skylarpiper710@gmail.com>
2018-12-08 19:13:55 -0600

    first push

---
+++ assets/javascript/game.js
@@ -0,0 +1,73 @@
+
+var compchoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+
+var wins = 0;
+var losses = 0;
+var gLeft = 9;
+var myGuess = [];
+
+var winText = document.getElementById("Wins");
+var lossesText = document.getElementById("Losses");
+var guessesText = document.getElementById("Guesses");
+var yourText = document.getElementById("Your")
+
+document.onkeyup =function(event){
+    var userChoice = event.key;
+
+    var computerChoice = compchoices[Math.floor(Math.random() * compchoices.length];
+
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+		 	
+
+			if (options.indexOf(userChoice) > -1) {
+
+				if (userChoice === computerChoice) {
+					wins++;
+					gLeft = 9;
+					myGuess = [];
+				}
+
+				if (userChoice != computerChoice) {
+					numGuesses --;
+                    guessChoices.push(userGuess);
+                    console.log(userGuess)
+				}
+
+				if (gLeft === 0) {
+
+				    gLeft = 9;
+				    losses ++;
+				    myGuess = [];
+
+				
+			    }
+            }
+
+
+
+
+
+
+            if (options.indexOf(userChoice) > -1) {
+
+				if (userChoice === computerChoice) {
+					wins++;
+					gLeft = 9;
+					myGuess = [];
+				}
+
+				if (userChoice != computerChoice) {
+					gLeft --;
+                    myGuess.push(userChoice);
+                    console.log(userChoice);
+				}
+
+				if (gLeft === 0) {
+
+				    gLeft = 9;
+				    losses ++;
+				    myGuess = [];
+
+				
+			    }
+            }
