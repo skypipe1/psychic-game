ba1371162d41c635826a048bd70050593fe2031c
Piper <skylarpiper710@gmail.com>
2018-12-09 15:06:14 -0600

    last push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,10 @@
 var compchoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
+
 var wins = 0;
 var losses = 0;
 var gLeft = 9;
 var myGuess = [];
-
-var winText = document.getElementById("Wins");
-var lossesText = document.getElementById("Losses");
-var guessesText = document.getElementById("Guesses");
-var yourText = document.getElementById("Your")
-
-
 
 
 document.onkeyup = function (event) {
@@ -23,15 +17,15 @@ document.onkeyup = function (event) {
     if (compchoices.indexOf(userChoice) > -1) {
 
         if (userChoice === computerChoice) {
-
-            // compchoices = computerchoices.indexOf
             
             gLeft = 9;
             wins++;
             myGuess = [];
+            
         }
 
         if (userChoice !== computerChoice) {
+           
             gLeft--;
             myGuess.push(userChoice);
 
@@ -43,7 +37,6 @@ document.onkeyup = function (event) {
             losses++;
             myGuess = [];
 
-
         }
         
 
@@ -53,10 +46,5 @@ document.onkeyup = function (event) {
     document.getElementById("Losses").innerHTML = ("Losses:" + losses);
     document.getElementById("Guesses").innerHTML = ("Guesses left:" + gLeft);
     document.getElementById("Your").innerHTML = ("Your guesses so far:" + myGuess);
-
-
-
-
-
 
 }
